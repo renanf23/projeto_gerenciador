eb9f4fc8c1afcc423bb0dc92800ee87b3724ee58
Renan Ferreira <101736583+renanf23@users.noreply.github.com>
2022-06-04 11:42:03 -0300

    att

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+
 
 function App() {
+  const nome = "Gustavo"
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
+      <SayMyName nome="Renan"/>
+      <SayMyName nome="Taiz"/>
+      <SayMyName nome={nome}/>
+      <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
