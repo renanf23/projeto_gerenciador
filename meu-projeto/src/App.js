75abbf610731d349c984c530f0a10be9b3d50fe3
Renan Ferreira <101736583+renanf23@users.noreply.github.com>
2022-06-04 17:56:54 -0300

    aula 09

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 
 function App() {
-  const nome = "Gustavo"
   return (
     <div className="App">
-      <SayMyName nome="Renan"/>
-      <SayMyName nome="Taiz"/>
-      <SayMyName nome={nome}/>
-      <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150"/>
+      <h1>Testando Eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
+      <Form />
     </div>
-  );
+  )
 }
 
 export default App;
