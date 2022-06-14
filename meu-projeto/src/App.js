fd6409e8db6b2ba27123da5a6be7f3d7013efdc9
Renan Ferreira <101736583+renanf23@users.noreply.github.com>
2022-06-14 12:34:20 -0300

    Navbar

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,7 +3,7 @@ import Home from './components/pages/Home';
 import Empresa from './components/pages/Empresa';
 import Contato from './components/pages/Contato';
 import { BrowserRouter as Router, Routes, Route,  } from 'react-router-dom';
-import Navbar from './components/layout/Navber';
+import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
 
